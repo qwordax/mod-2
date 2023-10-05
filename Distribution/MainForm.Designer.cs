e5a43575d1a010d99882912b80595634e4599b5c
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-10-05 10:02:45 +0300

    `feat:` implement dummy `distributionBox`

--- Distribution/MainForm.Designer.cs
+++ Distribution/MainForm.Designer.cs
@@ -5,16 +5,39 @@ namespace Distribution
 {
     public partial class MainForm
     {
+        private GroupBox distributionBox;
+
         private void InitializeComponent()
         {
+            distributionBox = new GroupBox();
+
+            distributionBox.SuspendLayout();
+
             SuspendLayout();
 
+            //
+            // distributionBox.
+            //
+            distributionBox.Location = new Point(5, 5);
+            distributionBox.Margin = new Padding(0, 0, 0, 0);
+            distributionBox.Name = "distributionBox";
+            distributionBox.Size = new Size(300, 590);
+            distributionBox.TabIndex = 0;
+            distributionBox.Text = "Distributions";
+
+            //
+            // MainForm.
+            //
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1200, 600);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             MaximizeBox = false;
             MinimizeBox = false;
             Text = "Distribution";
+
+            Controls.Add(distributionBox);
+
+            distributionBox.ResumeLayout(true);
 
             ResumeLayout(true);
         }
