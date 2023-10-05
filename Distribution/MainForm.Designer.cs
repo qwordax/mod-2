544b96c4251147cb761e106b23710566a2746fdc
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-10-05 09:55:47 +0300

    `feat:` add window settings

--- Distribution/MainForm.Designer.cs
+++ Distribution/MainForm.Designer.cs
@@ -1,10 +1,20 @@
-﻿namespace Distribution
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Distribution
 {
     public partial class MainForm
     {
         private void InitializeComponent()
         {
             SuspendLayout();
+
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1200, 600);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Text = "Distribution";
 
             ResumeLayout(true);
         }
