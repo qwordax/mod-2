135d6203d357e67ea6f790242e9d20024417da13
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-10-06 07:10:02 +0300

    `feat:` implement `Variation` class

---
+++ Distribution/Utilities/Variation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Distribution.Utilities
+{
+    /// <summary>
+    /// Represents a class of computing variation.
+    /// </summary>
+    public static class Variation
+    {
+        public static Double Compute(List<Double> values)
+        {
+            Double result = 0.0;
+
+            Double expect = Expectation.Compute(values);
+
+            foreach (Double value in values)
+            {
+                result += (value - expect) * (value - expect);
+            }
+
+            return result / values.Count;
+        }
+    }
+}
