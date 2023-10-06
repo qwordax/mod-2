77a6adcd2de324cbb042e7754329c9c6a8e7db01
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-10-06 07:09:43 +0300

    `feat:` implement `Expectation` class

---
+++ Distribution/Utilities/Expectation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Distribution.Utilities
+{
+    /// <summary>
+    /// Represents a class of computing expectation.
+    /// </summary>
+    public static class Expectation
+    {
+        public static Double Compute(List<Double> values)
+        {
+            Double result = 0.0;
+
+            foreach (Double value in values)
+            {
+                result += value;
+            }
+
+            return result / values.Count;
+        }
+    }
+}
