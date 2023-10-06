bf48debad44cc6b92a5980c328f900b0aa3ec173
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-10-06 10:20:32 +0300

    `feat:` implement `IDistributable` interface

---
+++ Distribution/Algorithms/IDistributable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Distribution.Algorithms
+{
+    /// <summary>
+    /// Defines a method to generate next <see cref="Double"/>
+    /// by distributions.
+    /// </summary>
+    public interface IDistributable
+    {
+        Double Next();
+    }
+}
