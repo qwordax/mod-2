548debdcc5a1323c9083ffa467e4884d1c71b457
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-10-06 07:20:49 +0300

    `feat:` implement `IRandomable` interface

---
+++ Distribution/Generators/IRandomable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Distribution.Generators
+{
+    /// <summary>
+    /// Defines a method to generate next <see cref="UInt64"/>
+    /// by pseudorandom number generators.
+    /// </summary>
+    public interface IRandomable
+    {
+        UInt64 Next();
+    }
+}
