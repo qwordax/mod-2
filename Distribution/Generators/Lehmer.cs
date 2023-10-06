85fb890ff8db67840800e6b6a2ddcdc17cff6539
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-10-06 15:31:03 +0300

    `fix!!!:` implement `Lehmer` instead of `LFSR`

---
+++ Distribution/Generators/Lehmer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Numerics;
+
+namespace Distribution.Generators
+{
+    /// <summary>
+    /// Represents a pseudorandom number generator based on
+    /// the Lehmer’s algorithm.
+    /// </summary>
+    public class Lehmer : IRandomable
+    {
+        private BigInteger A { get; set; }
+        private BigInteger M { get; set; }
+        private BigInteger X { get; set; }
+
+        public Lehmer(UInt64 a, UInt64 m, UInt64 x)
+        {
+            A = a;
+            M = m;
+            X = x;
+        }
+
+        public UInt64 Next()
+        {
+            X = A * X % M;
+            return (UInt64)(X & 0xFFFF_FFFF_FFFF_FFFF);
+        }
+    }
+}
