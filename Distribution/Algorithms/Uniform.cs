2fad4dadea6fb4130a08ee2a0a2acaed0dd56479
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-10-06 10:45:04 +0300

    `feat:` implement `Uniform` class

---
+++ Distribution/Algorithms/Uniform.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Distribution.Generators;
+
+namespace Distribution.Algorithms
+{
+    public class Uniform : IDistributable
+    {
+        private IRandomable R { get; set; }
+
+        private Double A { get; set; }
+        private Double B { get; set; }
+
+        public Uniform(IRandomable r, Double a, Double b)
+        {
+            R = r;
+            A = a;
+            B = b;
+        }
+
+        public Double Next()
+        {
+            Double r = (Double)R.Next() / UInt64.MaxValue;
+            return A + (B - A) * r;
+        }
+    }
+}
